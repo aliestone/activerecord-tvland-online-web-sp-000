b9662dd3b6a331f124cb283797161348058f4426
Alie Stone <helloaliestone@gmail.com>
2020-12-13 20:58:32 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,4 +4,10 @@ class Show < ActiveRecord::Base
   has_many :genres
   has_many :actors, through: :characters
 
+def actors_list
+  show = Show.name
+  character = Character.new
+  full_name = Actor.new
+  show.characters << character
+  show.save
 end
